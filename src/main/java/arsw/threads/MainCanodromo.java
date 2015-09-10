9cd3467182f947646fdae6b5044116a472b18ba8
Francisco José Cháves <pachopepe@gmail.com>
2015-09-09 22:20:19 -0500

    First commit

---
+++ src/main/java/arsw/threads/MainCanodromo.java
@@ -0,0 +1,73 @@
+package arsw.threads;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
+public class MainCanodromo {
+
+    private static Galgo[] galgos;
+
+    private static Canodromo can;
+
+    private static RegistroLlegada reg = new RegistroLlegada();
+
+    public static void main(String[] args) {
+        can = new Canodromo(17, 100);
+        galgos = new Galgo[can.getNumCarriles()];
+        can.setVisible(true);
+
+        //Acción del botón start
+        can.setStartAction(
+                new ActionListener() {
+
+                    @Override
+                    public void actionPerformed(final ActionEvent e) {
+						//como acción, se crea un nuevo hilo que cree los hilpos
+                        //'galgos', los pone a correr, y luego muestra los resultados.
+                        //La acción del botón se realiza en un hilo aparte para evitar
+                        //bloquear la interfaz gráfica.
+                        new Thread() {
+                            public void run() {
+                                ((JButton) e.getSource()).setEnabled(false);
+                                for (int i = 0; i < can.getNumCarriles(); i++) {
+                                    //crea los hilos 'galgos'
+                                    galgos[i] = new Galgo(can.getCarril(i), "" + i, reg);
+                                    //inicia los hilos
+                                    galgos[i].start();
+
+                                }
+                               
+				winnerDialog(reg.getGanador(),reg.getUltimaPosicionAlcanzada() - 1); 
+                                System.out.println("El ganador fue:" + reg.getGanador());
+
+                            }
+                        }.start();
+
+                    }
+                }
+        );
+
+        can.setStopAction(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.out.println("Carrera pausada!");
+                    }
+                }
+        );
+
+        can.setContinueAction(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.out.println("Carrera reanudada!");
+                    }
+                }
+        );
+
+    }
+
+}
